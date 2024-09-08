7db0b89685d260d8669498a9d2b06afd817beb81
wkalbous <wassim.kalboussi@capgemini.com>
2024-09-08 11:31:46 +0200

    First commit

---
+++ src/test/java/com/libontest/tendeuse/TendeuseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libontest.tendeuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TendeuseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
